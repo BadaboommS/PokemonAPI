749b21fcc2e1c2a4693f6269d5075cc12dbee556
BadaboommS <yann.badaboomms@gmail.com>
2024-04-19 00:51:49 +0200

    changes login.js

--- src/routes/login.js
+++ src/routes/login.js
@@ -8,11 +8,11 @@ module.exports = (app) => {
         User.findOne({ where: { username: req.body.username } }).then ((user) => {
 
             if(!user){
-                const message = 'L\'utilisateurdemandé n\'existe pas.'
+                const message = 'L\'utilisateur demandé n\'existe pas.'
                 return res.status(404).json({ message })
             }
 
-            bcrypt.compare(req.body.password, user.password).then(isPasswordValid => {
+            return bcrypt.compare(req.body.password, user.password).then(isPasswordValid => {
                 if(!isPasswordValid){
                     const message = `Le mot de passe est incorrect`
                     return res.status(401).json({ message })
